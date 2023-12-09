66035a184a9c18ffc3cfadd736ee460fac61e15e
Zidane <zidanesanjaya81@Gmail.com>
2023-12-09 13:44:55 +0700

    z

---
+++ resources/views/layouts/user/footer.blade.php
@@ -0,0 +1,60 @@
+<footer class="footer">
+    <div class="footer_container d-flex flex-xl-row flex-column align-items-start justify-content-start">
+        <div class="newsletter_container">
+            <div class="newsletter_title">
+                <h2>Subscribe to our newsletter</h2>
+            </div>
+            <form action="#" id="newsletter_form" class="newsletter_form">
+                <input type="email" class="newsletter_input" placeholder="Your E-mail" required="required">
+                <button class="newsletter_button">Subscribe</button>
+            </form>
+        </div>
+        <div class="footer_lists d-flex flex-sm-row  flex-column align-items-start justify-content-start ml-xl-auto">
+
+            <!-- Useful Links -->
+            <div class="footer_list">
+                <div class="footer_list_title">Useful Links</div>
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="about.html">About us</a></li>
+                    <li><a href="#">Testimonials</a></li>
+                    <li><a href="#">Music</a></li>
+                    <li><a href="blog.html">News</a></li>
+                </ul>
+            </div>
+
+            <!-- Mixtape -->
+            <div class="footer_list">
+                <div class="footer_list_title">Mixtape</div>
+                <ul>
+                    <li><a href="#">Music</a></li>
+                    <li><a href="#">PR & Marketing</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Testimonials</a></li>
+                </ul>
+            </div>
+
+            <!-- Connect -->
+            <div class="footer_list">
+                <div class="footer_list_title">Connect</div>
+                <ul>
+                    <li><a href="#">Returns</a></li>
+                    <li><a href="#">Shippings</a></li>
+                    <li><a href="#">Jobs</a></li>
+                    <li><a href="#">Social Media</a></li>
+                    <li><a href="#">Soundcloud</a></li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+    <div class="copyright_bar">
+        <span><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            Copyright &copy;<script>
+                document.write(new Date().getFullYear());
+            </script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+        </span>
+    </div>
+</footer>
