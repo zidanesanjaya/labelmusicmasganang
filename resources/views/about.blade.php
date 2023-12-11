84e9239eeac606cc93f3124691aa1684cbcb172b
Zidane <zidanesanjaya81@Gmail.com>
2023-12-11 19:49:14 +0700

    z

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -60,15 +60,67 @@
         <div class="home">
             <div class="home_inner">
                 <!-- Image artist: https://unsplash.com/@yoannboyer -->
-                <div class="parallax_background parallax-window" data-parallax="scroll" data-image-src="/mixtape-master/images/about.jpg" data-speed="0.8"></div>
+                <div class="parallax_background-home parallax-window-home" data-parallax="scroll" data-image-src="/mixtape-master/images/about.jpg" data-speed="0.8"></div>
                 <div class="home_container">
                     <div class="home_content text-center">
-                        <div class="home_subtitle">Just us</div>
-                        <div class="home_title">Discography</div>
+                        <div class="home_title">About</div>
+                        <div class="home_subtitle">us</div>
                     </div>
                 </div>
             </div>
         </div>
+
+
+        <!-- begin::about -->
+        <div class="about">
+            <div class="parallax_background-about parallax-window-about" data-parallax="scroll" data-image-src="https://www.trinityproduction.com/storage/uploads/2022/02/18/NP-15-of-24_uid_620f7afb671f8.jpg" data-speed="0.8"></div>
+            <div class="container">
+                <div class="row">
+
+                    <!-- About Content -->
+                    <div class="col-lg-7 offset-lg-5">
+                        <div class="about_content">
+                            <div class="section_title_container">
+                                <div class="section_title">
+                                    <h1>Company</h1>
+                                </div>
+                                <div class="section_subtitle">Information</div>
+                            </div>
+                            <div class="about_text">
+                                <p> In vitae nisi aliquam, scelerisque leo a, volutpat sem. Vivamus rutrum dui fermentum eros hendrerit, id lobortis leo volutpat. Maecenas sollicitudin est in libero pretium interdum. Nullam volutpat dui sem, ac congue purus luctus nec. Curabitur luctus luctus erat, sit amet facilisis quam congue quis. Quisque ornare luctus erat id dignissim. Nullam ac nunc quis ex porttitor luctus.Integer sed facilisis eros. In iaculis rhoncus velit in malesuada. In hac habitasse platea dictumst. Fusce erat ex, consectetur sit amet ornare suscipit, porta et erat. Donec nec nisi in nibh commodo laoreet. Mauris dapibus justo ut feugiat malesuada. Fusce ultricies ante tortor, non vestibulum est feugiat ut.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="detailabout">
+            <div class="parallax_background-detailabout parallax-window-detailabout" data-parallax="scroll" data-image-src="https://www.trinityproduction.com/storage/uploads/2021/12/24/mbuhsopo.png_uid_61c5b89f3c8a8.jpg" data-speed="0.8"></div>
+            <div class="container">
+                <div class="row">
+
+                    <!-- About Content -->
+                    <div class="col-lg-7 offset-lg-5">
+                        <div class="detailabout_content">
+                            <div class="detailabout_text">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis eaque, necessitatibus molestias dolores voluptas cumque minima. Minima enim sint, quaerat blanditiis suscipit animi perferendis tempore libero error, quidem, culpa saepe!
+                                Reiciendis eligendi perferendis, dolores distinctio, error, nostrum aspernatur repudiandae laboriosam debitis enim dignissimos vitae officia deleniti beatae? Nemo repellat libero, ullam facere at hic quas velit nulla corporis eveniet commodi?
+                                Quaerat obcaecati eius voluptatum quam reprehenderit libero inventore impedit reiciendis molestias. Tempore ad labore quaerat voluptas qui. Alias numquam ipsa omnis error nam a? Deleniti temporibus totam eligendi reiciendis nobis?
+                                Accusantium cum sequi quibusdam reprehenderit excepturi doloremque repellat aspernatur, omnis deleniti doloribus sunt voluptates necessitatibus facilis placeat id vel iste voluptatum? Perferendis earum libero ducimus natus blanditiis. Ut, reprehenderit architecto!
+                                Voluptatem cumque omnis expedita nisi quas quod. Ratione at provident porro molestias, reprehenderit facere dolorum asperiores molestiae accusantium earum modi velit debitis odio soluta! Placeat delectus nulla modi aspernatur odit.
+                                Ea cum nihil provident sunt sapiente, commodi itaque veritatis accusamus accusantium similique dolores vitae, aspernatur voluptates reprehenderit quod maiores rerum deserunt laboriosam. Quas, harum. Veritatis sint beatae eum blanditiis velit!
+                                Nam autem, nulla quasi recusandae esse molestias asperiores illo voluptates mollitia quos possimus libero nihil perspiciatis, ut ipsam aperiam itaque quas voluptate blanditiis obcaecati? Facere veniam non culpa dolore quam?
+                                Neque assumenda, accusantium error obcaecati reprehenderit, unde pariatur ea perspiciatis atque explicabo fugit ab soluta molestias praesentium eveniet itaque facere quos iusto excepturi ullam necessitatibus quae rerum architecto? Dolorum, tempora!
+                                Doloribus, porro temporibus voluptatum recusandae blanditiis dolorem, inventore sunt molestiae quae soluta adipisci magnam possimus aliquam earum, iure et? Corrupti voluptate eius magni deleniti veritatis nisi vero officiis perferendis explicabo.
+                                Vitae iste dolorem doloribus! Expedita, iste. Id pariatur aut quas perspiciatis voluptas dignissimos, quae itaque repudiandae dicta distinctio, reprehenderit ex necessitatibus? Obcaecati porro voluptatum tempore ipsum a et voluptatem deleniti.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- end::about -->
 
         <!-- Discs -->
 
@@ -183,79 +235,6 @@
                         </div>
                     </div>
 
-                </div>
-            </div>
-        </div>
-
-        <!-- Artist -->
-
-        <div class="artist">
-            <div class="parallax_background parallax-window" data-parallax="scroll" data-image-src="/mixtape-master/images/artist_2.jpg" data-speed="0.8"></div>
-            <div class="container">
-                <div class="row">
-
-                    <!-- Artist Content -->
-                    <div class="col-lg-7 offset-lg-5">
-                        <div class="artist_content">
-                            <div class="section_title_container">
-                                <div class="section_subtitle">Events</div>
-                                <div class="section_title">
-                                    <h1>The Artist</h1>
-                                </div>
-                            </div>
-                            <div class="artist_text">
-                                <p> In vitae nisi aliquam, scelerisque leo a, volutpat sem. Vivamus rutrum dui fermentum eros hendrerit, id lobortis leo volutpat. Maecenas sollicitudin est in libero pretium interdum. Nullam volutpat dui sem, ac congue purus luctus nec. Curabitur luctus luctus erat, sit amet facilisis quam congue quis. Quisque ornare luctus erat id dignissim. Nullam ac nunc quis ex porttitor luctus.</p>
-                                <p>Integer sed facilisis eros. In iaculis rhoncus velit in malesuada. In hac habitasse platea dictumst. Fusce erat ex, consectetur sit amet ornare suscipit, porta et erat. Donec nec nisi in nibh commodo laoreet. Mauris dapibus justo ut feugiat malesuada. Fusce ultricies ante tortor, non vestibulum est feugiat ut.</p>
-                            </div>
-                            <div class="artist_sig"><img src="/mixtape-master/images/sig.png" alt=""></div>
-                            <div class="single_player_container d-flex flex-column align-items-start justify-content-center">
-                                <div class="single_player">
-                                    <div id="jplayer_2" class="jp-jplayer"></div>
-                                    <div id="jp_container_2" class="jp-audio" role="application" aria-label="media player">
-                                        <div class="jp-type-single">
-                                            <div class="player_details d-flex flex-row align-items-center justify-content-start">
-                                                <div class="jp-details">
-                                                    <div>playing</div>
-                                                    <div class="jp-title" aria-label="title">&nbsp;</div>
-                                                </div>
-                                                <div class="jp-controls-holder ml-auto">
-                                                    <button class="jp-play" tabindex="0"></button>
-                                                </div>
-                                            </div>
-                                            <div class="player_controls">
-                                                <div class="jp-gui jp-interface d-flex flex-row align-items-center justify-content-start">
-                                                    <div class="jp-controls-holder time_controls d-flex flex-row align-items-center justify-content-start">
-                                                        <div>
-                                                            <div class="jp-current-time" role="timer" aria-label="time">&nbsp;</div>
-                                                        </div>
-                                                        <div class="jp-progress">
-                                                            <div class="jp-seek-bar">
-                                                                <div class="jp-play-bar"></div>
-                                                            </div>
-                                                        </div>
-                                                        <div>
-                                                            <div class="jp-duration ml-auto" role="timer" aria-label="duration">&nbsp;</div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="jp-volume-controls d-flex flex-row align-items-center justify-content-start ml-auto">
-                                                        <button class="jp-mute" tabindex="0"></button>
-                                                        <div class="jp-volume-bar">
-                                                            <div class="jp-volume-bar-value"></div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="jp-no-solution">
-                                                <span>Update Required</span>
-                                                To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
