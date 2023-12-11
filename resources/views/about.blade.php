5e2305e54e533b593b10b6f0baa86dd465039a01
Zidane <zidanesanjaya81@Gmail.com>
2023-12-11 20:01:16 +0700

    z

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -122,179 +122,37 @@
         </div>
         <!-- end::about -->
 
-        <!-- Discs -->
+        <!-- begin::visi dan misi -->
+        <div class="visimisi">
+            <div class="visimisi_container">
+                <div class="row d-flex justify-content-center align-items-center">
 
-        <div class="discs">
-            <div class="container">
-                <div class="row discs_row">
-
-                    <!-- Disc -->
-                    <div class="col-xl-4 col-md-6">
-                        <div class="disc">
-                            <a href="single.html">
-                                <div class="disc_image"><img src="/mixtape-master/images/disc_1.jpg" alt="https://unsplash.com/@tanelah"></div>
-                                <div class="disc_container">
-                                    <div>
-                                        <div class="disc_content_1">
-                                            <div class="disc_title">Mixtape</div>
-                                            <div class="disc_subtitle">Music For the People</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
+                    <!-- Visi -->
+                    <div class="col-xl-6 col-md-12">
+                        <div class="milestone d-flex flex-row align-items-center justify-content-center">
+                            <div class="milestone_content">
+                                <h1>
+                                    Visi
+                                </h1>
+                            </div>
                         </div>
                     </div>
 
-                    <!-- Disc -->
-                    <div class="col-xl-4 col-md-6">
-                        <div class="disc">
-                            <a href="single.html">
-                                <div class="disc_image"><img src="/mixtape-master/images/disc_2.jpg" alt="https://unsplash.com/@kasperrasmussen"></div>
-                                <div class="disc_container">
-                                    <div>
-                                        <div class="disc_content_2 d-flex flex-column align-items-center justify-content-center">
-                                            <div>
-                                                <div class="disc_title">Mixtape</div>
-                                                <div class="disc_subtitle">the world is ours</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-
-                    <!-- Disc -->
-                    <div class="col-xl-4 col-md-6">
-                        <div class="disc">
-                            <a href="single.html">
-                                <div class="disc_image"><img src="/mixtape-master/images/disc_3.jpg" alt="https://unsplash.com/@photones11"></div>
-                                <div class="disc_container">
-                                    <div>
-                                        <div class="disc_content_3">
-                                            <div>
-                                                <div class="disc_title">Mixtape</div>
-                                                <div class="disc_subtitle">Singles</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-
-                    <!-- Disc -->
-                    <div class="col-xl-4 col-md-6">
-                        <div class="disc">
-                            <a href="single.html">
-                                <div class="disc_image"><img src="/mixtape-master/images/disc_4.jpg" alt="https://unsplash.com/@rexcuando"></div>
-                                <div class="disc_container">
-                                    <div>
-                                        <div class="disc_content_4 d-flex flex-column align-items-start justify-content-end">
-                                            <div class="text-left">
-                                                <div class="disc_title">Mixtape</div>
-                                                <div class="disc_subtitle">1983</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-
-                    <!-- Disc -->
-                    <div class="col-xl-4 col-md-6">
-                        <div class="disc">
-                            <a href="single.html">
-                                <div class="disc_image"><img src="/mixtape-master/images/disc_5.jpg" alt="https://unsplash.com/@alicemoore"></div>
-                                <div class="disc_container">
-                                    <div>
-                                        <div class="disc_content_5">
-                                            <div class="disc_title">Mixtape</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-
-                    <!-- Disc -->
-                    <div class="col-xl-4 col-md-6">
-                        <div class="disc">
-                            <a href="single.html">
-                                <div class="disc_image"><img src="/mixtape-master/images/disc_6.jpg" alt="https://unsplash.com/@arstyy"></div>
-                                <div class="disc_container">
-                                    <div>
-                                        <div class="disc_content_6">
-                                            <div class="disc_title">Mixtape</div>
-                                            <div class="disc_subtitle">Music For the People</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
+                    <!-- Misi -->
+                    <div class="col-xl-6 col-md-12">
+                        <div class="milestone d-flex flex-row align-items-center justify-content-center">
+                            <div class="milestone_content">
+                                <h1>
+                                    Misi
+                                </h1>
+                            </div>
                         </div>
                     </div>
 
                 </div>
             </div>
         </div>
-
-        <!-- Milestones -->
-
-        <div class="milestones">
-            <div class="milestones_container">
-                <div class="parallax_background parallax-window" data-parallax="scroll" data-image-src="/mixtape-master/images/milestones.jpg" data-speed="0.8"></div>
-                <div class="container">
-                    <div class="row milestones_row">
-
-                        <!-- Milestone -->
-                        <div class="col-xl-3 col-md-6 milestone_col">
-                            <div class="milestone d-flex flex-row align-items-center justify-content-start">
-                                <div class="milestone_icon"><img src="/mixtape-master/images/icon_1.svg" alt="https://www.flaticon.com/authors/smashicons"></div>
-                                <div class="milestone_content">
-                                    <div class="milestone_counter" data-end-value="298" data-sign-after="k">0</div>
-                                    <div class="milestone_text">Albums sold</div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Milestone -->
-                        <div class="col-xl-3 col-md-6 milestone_col">
-                            <div class="milestone d-flex flex-row align-items-center justify-content-start">
-                                <div class="milestone_icon"><img src="/mixtape-master/images/icon_2.svg" alt="https://www.flaticon.com/authors/smashicons"></div>
-                                <div class="milestone_content">
-                                    <div class="milestone_counter" data-end-value="183">0</div>
-                                    <div class="milestone_text">Live Concerts</div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Milestone -->
-                        <div class="col-xl-3 col-md-6 milestone_col">
-                            <div class="milestone d-flex flex-row align-items-center justify-content-start">
-                                <div class="milestone_icon"><img src="/mixtape-master/images/icon_3.svg" alt="https://www.flaticon.com/authors/smashicons"></div>
-                                <div class="milestone_content">
-                                    <div class="milestone_counter" data-end-value="37">0</div>
-                                    <div class="milestone_text">Awards won</div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Milestone -->
-                        <div class="col-xl-3 col-md-6 milestone_col">
-                            <div class="milestone d-flex flex-row align-items-center justify-content-start">
-                                <div class="milestone_icon"><img src="/mixtape-master/images/icon_4.svg" alt="https://www.flaticon.com/authors/smashicons"></div>
-                                <div class="milestone_content">
-                                    <div class="milestone_counter" data-end-value="14">0</div>
-                                    <div class="milestone_text">New Singles</div>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
+        <!-- end::visi dan misi -->
 
     </div>
     @include('layouts.user.footer')
