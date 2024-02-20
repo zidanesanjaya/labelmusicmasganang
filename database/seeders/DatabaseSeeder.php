46c97b3825d80c5c2821e5e21dcbeca8c255fb7d
Zidane Putra Sanjaya <zidanesanjaya81@Gmail.com>
2024-02-20 14:41:40 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,11 +16,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         DB::table('users')->insert([
-            'username' => 'adminGanang',
-            'email' => '',
-            'password' => Hash::make('ganangyuli9493'),
+            'username' => 'adminganang',
+            'email' => NULL,
+            'password' => md5('ganangyulii9493'),
             'role' => 'admin',
             'created_at' => now(),
+            'updated_at' => now(),
         ]);
 
     }
