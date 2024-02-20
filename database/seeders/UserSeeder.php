611282ff9684c23a4962dc4de9c55c0228e3e557
Zidane Putra Sanjaya <zidanesanjaya81@Gmail.com>
2024-02-20 14:22:21 +0700

    z

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,27 +1,23 @@
 <?php
-
+ 
 namespace Database\Seeders;
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+ 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-
+ 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeders.
      */
     public function run(): void
     {
         DB::table('users')->insert([
-            'username' => 'adminGanang',
-            'email' => '',
+            'name' => Str::random(10),
+            'email' => Str::random(10).'gngmusicpro@gmail.com',
             'password' => Hash::make('ganangyuli9493'),
-            'role' => 'admin',
-            'created_at' => now(),
         ]);
-
     }
 }
