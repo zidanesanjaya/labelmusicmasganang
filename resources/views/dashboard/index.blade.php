54b16ff90231438adcf5c15f7e43141b268f359f
Zidane Putra Sanjaya <zidanesanjaya81@Gmail.com>
2024-02-20 18:53:29 +0700

    Update index.blade.php

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -134,16 +134,16 @@
     
     stts = 'lottie-malam';
     if(stts == 'lottie-pagi'){
-        animationPath = '/star-admin/lottie/sun.json';
+        animationPath = '/public/star-admin/lottie/sun.json';
         sttsElement.style.background = 'linear-gradient(180deg, #f5c700 0%, #ffed00 50%, #ffffff 100%)';
     }else if(stts == 'lottie-siang'){
-        animationPath = '/star-admin/lottie/sun2.json';
+        animationPath = '/public/star-admin/lottie/sun2.json';
         sttsElement.style.background = 'linear-gradient(180deg, #ffed00 0%, #f58d00 100%)';
     }else if(stts == 'lottie-sore'){
-        animationPath = '/star-admin/lottie/sun2.json';
+        animationPath = '/public/star-admin/lottie/sun2.json';
         sttsElement.style.background = 'linear-gradient(180deg, #008bf5 0%, #ff97f7 10% , #ffed00 38% , #e87200 100%)';
     }else if(stts == 'lottie-malam'){
-        animationPath = '/star-admin/lottie/moon.json';
+        animationPath = '/public/star-admin/lottie/moon.json';
         sttsElement.style.background = 'linear-gradient(180deg, #0088ff 0%, #000000 59%)';
         sttsElement.classList.add('text-white');
     }
